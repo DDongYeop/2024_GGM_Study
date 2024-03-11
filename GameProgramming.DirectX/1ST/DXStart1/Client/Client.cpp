42a30a3c65588c630545dd89203952b76e8a6dfd
Dongyeop <ddongyeop0129@gmail.com>
2024-03-11 15:37:03 +0900

    GameProgramming.DirectX - Init Setting, WinMain

--- GameProgramming.DirectX/1ST/DXStart1/Client/Client.cpp
+++ GameProgramming.DirectX/1ST/DXStart1/Client/Client.cpp
@@ -1,8 +1,11 @@
 ﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+#include "pch.h"
 #include "framework.h"
 #include "Client.h"
+
+#include "Game.h"
 
 #define MAX_LOADSTRING 100
 
@@ -42,14 +45,34 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
+    // 스마트 포인터로 Game클래스의 메모리를 할당
+    unique_ptr<Game> game = make_unique<Game>();
+    game->Init();
+
     // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    /*while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
+    }*/
+
+    //게임 메세지 루프
+    while (true)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+
+        //엔진쪽 업데이트
+        game->Update();
     }
 
     return (int) msg.wParam;
@@ -76,7 +99,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CLIENT));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CLIENT);
+    wcex.lpszMenuName   = nullptr;
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
--- GameProgramming.DirectX/1ST/DXStart1/Engine/EnginePch.cpp
+++ GameProgramming.DirectX/1ST/DXStart1/Engine/EnginePch.cpp
@@ -4,5 +4,4 @@
 void HelloEngine()
 {
 	// 이 함수를 client에서 호출하는게 1차 목표 
-
 }
