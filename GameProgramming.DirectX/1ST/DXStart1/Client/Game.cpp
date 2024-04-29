aa1f2f11c9571d9720c87516cf96c17b4bbaeba0
Dongyeop <ddongyeop0129@gmail.com>
2024-04-29 17:58:29 +0900

    GameProgramming.DirectX - Triangle

--- GameProgramming.DirectX/1ST/DXStart1/Client/Game.cpp
+++ GameProgramming.DirectX/1ST/DXStart1/Client/Game.cpp
@@ -13,19 +13,58 @@ void Game::Init(const WindowInfo& wInfo)
     //삼각형 띄우기 테스트 코드
 
     // 힌트 정점 3개를 만드시오
-    Vertex triangle[] = {
-        { {0.0f,0.5f,0.0f},{1.f,0.f,0.f,1.f}},
-        { {0.5f,-0.5f,0.0f},{1.f,1.f,0.f,1.f}},
-        { {-0.5f,-0.5f,0.0f},{1.f,0.f,1.f,1.f}},
-    };
 
-    vector<Vertex> v;
-    for (int i = 0; i < 3; i++)
-        v.push_back(triangle[i]);
+    /*vector<Vertex> vec(3);
 
+    vec[0].pos = Vec3(0, 0.5f, 0.5f);
+    vec[0].color = Vec4(1.0f, 0.f, 0.f, 1.0f);
 
-    // 물리적인 파일의 위치에서 쉐이더 파일을 읽어오시오
-    mesh->Init(v);
+    vec[1].pos = Vec3(0.5f, -0.5f, 0.5f);
+    vec[1].color = Vec4(0, 1.f, 0.f, 1.f);
+    
+    vec[2].pos = Vec3(-.5f, -.5f, .5f);
+    vec[2].color = Vec4(0, 0.f, 1.f, 1.0f);*/
+
+    // VBV 버텍스 6개로 사각형 그리기 코드
+    /*vector<Vertex> vec(6);
+    vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+    vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+    vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+    vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+    vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+    vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+
+    vec[3].pos = Vec3(0.5f, -0.5f, 0.5f);
+    vec[3].color = Vec4(0.f, 0.f, 1.f, 1.f);
+    vec[4].pos = Vec3(-0.5f, -0.5f, 0.5f);
+    vec[4].color = Vec4(0.f, 1.f, 0.f, 1.f);
+    vec[5].pos = Vec3(-0.5f, 0.5f, 0.5f);
+    vec[5].color = Vec4(1.f, 0.f, 0.f, 1.f);*/
+
+    // VBV + IBV로 사각형 그려보기 코드
+    vector<Vertex> vec(4);
+    vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+    vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+    vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+    vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+    vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+    vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+    vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+    vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
+
+    vector<uint32> indexVec;
+    {
+        indexVec.push_back(0);
+        indexVec.push_back(1);
+        indexVec.push_back(2);
+    }
+    {
+        indexVec.push_back(0);
+        indexVec.push_back(2);
+        indexVec.push_back(3);
+    }
+
+    mesh->Init(vec, indexVec);
     shader->Init(L"..\\Resources\\Shader\\default.hlsli");
     
     GEngine->GetCmdQueue()->WaitSync();
@@ -42,16 +81,24 @@ void Game::Update()
     /*mesh->Render();*/
 
     //삼각형1
+    //{
+    //    Transform t;
+    //    t.offset = Vec4(0.75f, 0.f, 0.f, 0.f);
+    //    mesh->SetTransform(t);
+    //    mesh->Render();
+    //}
+    ////삼각형2
+    //{
+    //    Transform t;
+    //    t.offset = Vec4(0.f, 0.75f, 0.f, 0.f);
+    //    mesh->SetTransform(t);
+    //    mesh->Render();
+    //}
+
+    //VBV
     {
         Transform t;
-        t.offset = Vec4(0.75f, 0.f, 0.f, 0.f);
-        mesh->SetTransform(t);
-        mesh->Render();
-    }
-    //삼각형2
-    {
-        Transform t;
-        t.offset = Vec4(0.f, 0.75f, 0.f, 0.f);
+        t.offset = Vec4(0.f, 0.f, 0.f, 0.f);
         mesh->SetTransform(t);
         mesh->Render();
     }
