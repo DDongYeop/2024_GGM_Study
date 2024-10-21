141edb8971a9e1f869edff32b339dac1aea167bf
Dongyeop <ddongyeop0129@gmail.com>
2024-10-21 21:01:59 +0900

    DX - Editor - Assembly

---
+++ GameProgramming.DirectX/2ND/EditorEx_1/Assets/01.Scripts/TestScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Reflection;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TestScript : MonoBehaviour
+{
+    private void OnEnable() 
+    {
+#if UNITY_EDITOR
+        var tex = EditorGUIUtility.Load("logo.png") as Texture;
+        Debug.Log(tex);
+#endif
+    }
+    
+    [InitializeOnLoadMethod]
+    static void GetBultinAssetNames()
+    {
+        var flags = BindingFlags.Static | BindingFlags.NonPublic;
+        var info = typeof(EditorGUIUtility).GetMethod("GetEditorAssetBundle", flags);
+        var bundle = info.Invoke(null, new object[0]) as AssetBundle;
+        foreach (var n in bundle.GetAllAssetNames())
+        {
+            Debug.Log(n);
+        }
+    }
+}
